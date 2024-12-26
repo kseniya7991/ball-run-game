2ccc03086936fa87deb1e6626034a972911cf7a7
kseniya7991 <kseniya7991@gmail.com>
2024-12-26 16:53:20 +0300

    fix lights

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,13 +1,19 @@
 import { useRapier, RigidBody } from "@react-three/rapier";
 import { useFrame } from "@react-three/fiber";
 import { useKeyboardControls } from "@react-three/drei";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
+import * as THREE from "three";
 
 export default function Player() {
     const body = useRef();
     const [subscribeKeys, getKeys] = useKeyboardControls();
     const { rapier, world } = useRapier();
+    const [smoothCameraPosition] = useState(() => new THREE.Vector3(10,10,10));
+    const [smoothCameraTarget] = useState(() => new THREE.Vector3());
 
+    /**
+     * Jump
+     */
     const jump = () => {
         const origin = body.current.translation();
         origin.y -= 0.31;
@@ -19,11 +25,13 @@ export default function Player() {
     };
 
     useEffect(() => {
+        /**
+         * Subscribe to jump event
+         */
         const unsubscribeJump = subscribeKeys(
-            //selector
-            (state) => state.jump,
-            //what to do
+            (state) => state.jump, // selector
             (val) => {
+                // what to do
                 if (val) jump();
             }
         );
@@ -33,6 +41,9 @@ export default function Player() {
         };
     }, []);
 
+    /**
+     * Controls
+     */
     useFrame((state, delta) => {
         const { forward, backward, leftward, rightward, jump } = getKeys();
         const impulse = { x: 0, y: 0, z: 0 };
@@ -62,6 +73,29 @@ export default function Player() {
         body.current?.applyTorqueImpulse(torque);
     });
 
+    /**
+     * Camera
+     */
+    useFrame((state, delta) => {
+        const bodyPosition = body.current?.translation();
+        if (!bodyPosition) return;
+
+        const cameraPosition = new THREE.Vector3();
+        cameraPosition.copy(bodyPosition);
+        cameraPosition.z += 3.25;
+        cameraPosition.y += 0.8;
+
+        const cameraTarget = new THREE.Vector3();
+        cameraTarget.copy(bodyPosition);
+        cameraTarget.y += 0.25;
+
+        smoothCameraPosition.lerp(cameraPosition, 5 * delta);
+        smoothCameraTarget.lerp(cameraTarget, 5 * delta);
+
+        state.camera.position.copy(smoothCameraPosition);
+        state.camera.lookAt(smoothCameraTarget);
+    });
+
     return (
         <RigidBody
             ref={body}
@@ -70,8 +104,8 @@ export default function Player() {
             colliders="ball"
             restitution={0.2}
             friction={1}
-            linearDamping={1}
-            angularDamping={1}>
+            linearDamping={0.5}
+            angularDamping={0.5}>
             <mesh castShadow>
                 <icosahedronGeometry args={[0.3, 1]} />
                 <meshStandardMaterial flatShading color="orangered" />
