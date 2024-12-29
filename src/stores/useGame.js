33d2993a194642d46de3b4bcae8f9e4a9055e538
kseniya7991 <kseniya7991@gmail.com>
2024-12-29 20:40:34 +0300

    add updating time

--- src/stores/useGame.js
+++ src/stores/useGame.js
@@ -5,15 +5,22 @@ export default create(
     subscribeWithSelector((set) => {
         return {
             blocksCount: 3,
+            /**
+             * Time
+             */
+            startTime: 0,
+            endTime: 0,
 
-            // Phases
+            /**
+             * Phases
+             */
             phase: "ready",
-            start: () => set((state) => (state.phase === "ready" ? { phase: "playing" } : {})),
+            start: () => set((state) => (state.phase === "ready" ? { phase: "playing", startTime: Date.now() } : {})),
             restart: () =>
                 set((state) =>
                     state.phase === "playing" || state.phase === "ended" ? { phase: "ready" } : {}
                 ),
-            end: () => set((state) => (state.phase === "playing" ? { phase: "ended" } : {})),
+            end: () => set((state) => (state.phase === "playing" ? { phase: "ended", endTime: Date.now() } : {})),
         };
     })
 );
