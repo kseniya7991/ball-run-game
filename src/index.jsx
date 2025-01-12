92425cb52911f895bec3d721d0c7d409c3d45d59
kseniya7991 <kseniya7991@gmail.com>
2025-01-12 22:26:02 +0300

    format file

--- src/index.jsx
+++ src/index.jsx
@@ -8,13 +8,14 @@ import Interface from "./Interface";
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
-    <KeyboardControls map={[
-        {name:"forward",keys: ['ArrowUp','KeyW']},
-        {name:"backward",keys: ['ArrowDown','KeyS']},
-        {name:"leftward",keys: ['ArrowLeft','KeyA']},
-        {name:"rightward",keys: ['ArrowRight','KeyD']},
-        {name:"jump",keys: ['Space']},
-    ]}>
+    <KeyboardControls
+        map={[
+            { name: "forward", keys: ["ArrowUp", "KeyW"] },
+            { name: "backward", keys: ["ArrowDown", "KeyS"] },
+            { name: "leftward", keys: ["ArrowLeft", "KeyA"] },
+            { name: "rightward", keys: ["ArrowRight", "KeyD"] },
+            { name: "jump", keys: ["Space"] },
+        ]}>
         <Canvas
             shadows
             camera={{
