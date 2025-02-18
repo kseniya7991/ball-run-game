ebb01b7676f19ceb0a387462aacbe6836f1a360b
kseniya7991 <kseniya7991@gmail.com>
2025-02-18 15:32:39 +0300

    add spot light to finish scene

--- src/Lights.jsx
+++ src/Lights.jsx
@@ -1,28 +1,37 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect } from "react";
 import { useFrame } from "@react-three/fiber";
+import useGame from "./stores/useGame";
 
 export default function Lights({ isFinish = false }) {
     const light = useRef();
+    const spotLight = useRef();
+    const targetRef = useRef();
+    const finalLevelLength = useGame((state) => state.finalLevelLength);
 
     useFrame((state) => {
         // Update Z
         light.current.position.z = state.camera.position.z + 1 - 4;
         light.current.target.position.z = state.camera.position.z - 4;
-
         // Update X
         light.current.position.x = state.camera.position.x + 6;
         light.current.target.position.x = state.camera.position.x + 4;
-
         light.current.target.updateMatrixWorld();
     });
 
     useEffect(() => {
         if (isFinish) {
             light.current.shadow.camera.far = 100;
+            light.current.intensity = 3;
             light.current.shadow.camera.updateProjectionMatrix();
             light.current.shadow.map.needsUpdate = true;
+
+            spotLight.current.target = targetRef.current;
         }
     }, [isFinish]);
+
+    useEffect(() => {
+       
+    }, []);
 
     return (
         <>
@@ -37,16 +46,28 @@ export default function Lights({ isFinish = false }) {
                 shadow-camera-top={10}
                 shadow-camera-right={10}
                 shadow-camera-bottom={-10}
-                shadow-camera-left={-10}></directionalLight>
+                shadow-camera-left={-10}
+            />
 
-            {/* <spotLight
-                position={[5, 10, 0]}
-                intensity={6}
-                distance={20}
-                angle={0.3}
-                penumbra={0.5}
-                decay={0}
-            /> */}
+            {isFinish && (
+                <group position={[4.48, -10 + 12, -finalLevelLength - 4]}>
+                    <spotLight
+                        ref={spotLight}
+                        position={[0, 0, 0]}
+                        intensity={10}
+                        distance={20}
+                        angle={0.3}
+                        penumbra={0.3}
+                        decay={0.2}
+                        color="#FFF7E1"
+                    />
+                    <object3D
+                        ref={targetRef}
+                        position={[-0.4, -1, 0]}
+                    />
+                </group>
+            )}
+
             <ambientLight intensity={1} />
         </>
     );
